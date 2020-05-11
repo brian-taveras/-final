d8924f6b8c72f1506101d2ba315a01a6f00069d1
fazt <A19A88603@educacion.edu.do>
2020-05-11 14:42:24 -0400

    funcionamiento de la base de datos y del login

--- conexion.php
+++ conexion.php
@@ -1,13 +1,30 @@
-<?php
-
-  $user = 'root';
-  $pass = '';
-  $db = 'final';
-
-  $db = new mysqli('localhost', $user, $pass, $db) or die("unable to connect");
 
 
-  echo "Hello wordls!!!";
+ <?php
 
+    class conexion{
 
- ?>
+        private $con;
+
+        public function __construct(){
+
+           
+          $this->con = new mysqli('localhost', 'root', '', 'final');
+
+        }
+
+        public function getUsers($user, $pass){
+          $query = $this->con->query("SELECT * FROM politecnico WHERE username = '".$user."' and password = '".$pass."';");
+            $retorno = [];
+            $i =0;
+
+            while ($fila = $query->fetch_assoc()){
+                $retorno [$i] =$fila;
+                $i++;                
+            }
+            return $retorno;
+        }
+    
+    }
+
+?>
